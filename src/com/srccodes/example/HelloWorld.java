64cec26bd0f9f478771629ac6733789516f2ce73
suchita26 <suchita2610@gmail.com>
2021-03-13 00:01:06 +0530

    first commit

---
+++ src/com/srccodes/example/HelloWorld.java
@@ -0,0 +1,17 @@
+
+
+package com.srccodes.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+     //  WebDriver driver = new WebDriver();
+       private static WebDriver driver = new FirefoxDriver();
+	}
+
+}
